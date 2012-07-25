638f6f42a9fd41cc18564930e49f3c78d02de380
suzhen <suzhen3001@gmail.com>
2012-07-26 01:53:19 +0800

    完成BUTTON

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20120720073020) do
     t.string   "name"
     t.string   "kind"
     t.string   "setting"
-    t.date     "startdate",   :default => '2012-07-23'
+    t.date     "startdate",   :default => '2012-07-25'
     t.date     "enddate"
     t.integer  "hits"
     t.integer  "clicks",      :default => 0
@@ -81,8 +81,8 @@ ActiveRecord::Schema.define(:version => 20120720073020) do
     t.integer  "parent_id",   :default => 0
     t.string   "description"
     t.integer  "listorder",   :default => 0
-    t.text     "arrparentid"
-    t.text     "arrchildid"
+    t.string   "arrparentid"
+    t.string   "arrchildid"
     t.integer  "hits"
     t.string   "style"
     t.string   "path"
@@ -137,7 +137,6 @@ ActiveRecord::Schema.define(:version => 20120720073020) do
     t.text     "body"
     t.text     "formatted_html"
     t.string   "category"
-    t.string   "content"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
