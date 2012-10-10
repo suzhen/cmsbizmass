0c6a21fdb8cf95a6654896e4b0a8b71e002b65ca
suzhen <sz3001@gmail.com>
2012-10-10 14:53:22 +0800

    检索文章

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,4 @@
+#coding: utf-8 
 class CategoriesController < ApplicationController
   before_filter :authenticate_user!  
 
@@ -47,7 +48,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to @category, notice: '分类创建成功。' }
         format.json { render json: @category, status: :created, location: @category }
       else
         format.html { render action: "new" }
@@ -63,7 +64,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to @category, notice: '分类更新成功。' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
